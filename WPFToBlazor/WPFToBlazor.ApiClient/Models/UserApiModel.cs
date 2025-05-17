48470413f312b4cc25b2c02942aeee04491deb1b
Rudolph Kalp <rudolphk@lightstone.co.za>
2025-05-17 10:24:21 +0200

    Add client api wrapper

---
+++ WPFToBlazor/WPFToBlazor.ApiClient/Models/UserApiModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFToBlazor.ApiClient.Models
+{
+    public class UserApiModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int IsAdmin { get; set; }
+        public string Password { get; set; }
+        public string CashierId { get; set; }
+        public string ProfilePicture { get; set; }
+
+        public UserApiModel()
+        {
+            
+        }
+    }
+}
