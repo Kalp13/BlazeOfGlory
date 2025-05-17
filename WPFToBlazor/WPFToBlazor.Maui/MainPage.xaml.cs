331df0da882033213135a15ce0f12325a5f8246c
Rudolph Kalp <rudolphk@lightstone.co.za>
2025-05-17 11:26:03 +0200

    Add new .NET MAUI application and UI components
    This commit introduces a new .NET MAUI application named `WPFToBlazor.Maui`, including essential files such as `App.xaml`, `MainPage.xaml`, and `MauiProgram.cs` to establish the application's structure and functionality. New UI components like `NavMenu.razor` and `MainLayout.razor` enhance navigation and layout. The project is configured to support multiple platforms, including Android and iOS, with the addition of platform-specific files like `AndroidManifest.xml` and `Info.plist`.
    
    Additionally, several SVG, HTML, and CSS files have been added to improve the visual and functional aspects of the application. The `AboutAssets.txt` file provides instructions for deploying raw assets, while the `splash.svg` file defines a splash screen. The `index.html` file sets up the basic structure of the web application, and `app.css` defines styles for various components. Updates to `bootstrap.min.css.map` enhance debugging capabilities for the Bootstrap framework.

---
+++ WPFToBlazor/WPFToBlazor.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WPFToBlazor.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
